7a7bcf1c9ea39812f2d1195a7c9cc46ba447e7bb
chouotter <easonchou15217@gmail.com>
2024-10-29 22:37:25 +0800

    typescript123

---
+++ TypeScript1.js
@@ -0,0 +1,7 @@
+var age = 25;
+
+var username = "Alice";
+
+username = "Bob";
+console.log("Age:", age);
+console.log("Username:", username);
---
+++ TypeScript1.ts
@@ -0,0 +1,7 @@
+const age: number = 25;
+
+let username: string = "Alice";
+username = "Bob";
+
+console.log("Age:", age);
+console.log("Username:", username);
